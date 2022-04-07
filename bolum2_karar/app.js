e56d63b343f70092e99d3248e697bf3f6f472f82
emine <keles.emine88@gmail.com>
2022-04-07 12:42:28 +0300

    karar

---
+++ bolum2_karar/app.js
@@ -0,0 +1,48 @@
+/*const sayi1 = Number(prompt("Birinci sayi: "));
+const islem = prompt("islemi giriniz :");
+const sayi2 = +prompt("Ikinci sayi: "); //number old soylemek icin + da kullanılır
+let sonuc = 0;
+
+if (islem == "+") {
+  sonuc = sayi2 + sayi1;
+} else if (islem == "-") {
+  sonuc = sayi1 - sayi2;
+} else if (islem == "*") {
+  sonuc = sayi2 * sayi1;
+} else if (islem == "/") {
+    if(!sayi2){   //sayi2 0,null,NaN gibi false degerler mi 
+        console.log("sayiyi 0 a bolemezsiniz")
+    }else{
+        sonuc = sayi1 / sayi2;
+    }
+  
+} else {
+  alert("yanlis islem yaptiniz ..");
+}
+console.log(`${sayi1} ${islem} ${sayi2} = ${sonuc}`);*/
+/*const age = prompt("yasinizi giriniz : ");
+const healt = prompt("saglikli misiniz e/h");
+alert(age > 18 && healt == "e" ? "askere gidebilirsin" : "askere gidemezsin");*/
+
+/*const maas = prompt("maas : ");
+const calismaSure = prompt("calisma sureniz : ");
+const yeniMaas =
+  calismaSure < 11 ? (calismaSure <= 5 ? maas * 1.1 : maas * 1.2) : maas * 1.5;
+console.log(`yeni maasiniz : ${yeniMaas}`);*/
+
+/*   donguler     */
+/*for (let i = 1; i <= 10; i++) {
+  console.log(`${i} mehmet`);
+}*/
+//asal sayi tespiti
+/*
+const sayi = +prompt("sayiyi giriniz :");
+let asal = true;
+for (let i = 2; i < sayi; i++) {
+  if (sayi % i == 0) {
+    asal = false;
+    break;
+  }
+  asal = true;
+}
+console.log(asal ? "sayi asaldir" : "sayi asal degildir");*/
